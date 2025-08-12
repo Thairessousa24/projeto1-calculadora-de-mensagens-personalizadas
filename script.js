9cb34b34fa7cebe49e0750871ea75ea865e1da6e
Thaires Sousa <thaires.rodrigues@aluno.ce.gov.br>
2025-08-12 11:29:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let nome ="Thaires"
+let idade = 15
+let cidade ="General Sampaio"
+let profissão ="RH operacional"
+let anoAtual = 2025
+
+let anodeNascimento = anoAtual - idade
+
+
+let salario = 2500
+let bonus = 4000
+
+let totaldoMes = salario + bonus
+
+alert(`========= Relatório do Usuário =========
+Nome : ${nome}
+Idade : ${idade}
+Cidade : ${cidade}
+Profissão : ${profissão}
+Ano de Nascimento : ${anoAtual - idade}
+
+Salario : U$${salario}
+Bonus : U$${bonus}
+Total do Mes : U$${totaldoMes}
+Total do Ano : U$${totaldoMes * 12}
+Total do Ano (salario dobrado) : U$${totaldoMes * 12 * 2}
+
+==============================
+
+`)
+
+
